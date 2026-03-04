6619bf47c30723a3dfdc9572579f307c9a4543ba
Mockbot Dev <dev@mockbot.local>
2026-03-04 08:41:16 -0800

    (chore): Remove unneeded debug tests, scripts, and logs

--- test_pt.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from prompt_toolkit.patch_stdout import patch_stdout
-
-async def main():
-    with patch_stdout():
-        print("\x1b[32mThis should be green\x1b[0m")
-        await asyncio.sleep(1)
-
-asyncio.run(main())
--- test_tts.py
+++
@@ -1,3 +0,0 @@
-import sys, os
-from bot.tts import process_text_thread
-process_text_thread('test message', 'test_channel', './messages.db', 'static/outputs/test/test.wav', '2026-02-27T08:00:00', 'test_msg_id', 'v2/en_speaker_5')
